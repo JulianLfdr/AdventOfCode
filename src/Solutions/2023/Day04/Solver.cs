a933d1058579082a951d5f73c7c3d48f156bb68f
Julian Lefondeur <33429486+JulianLfdr@users.noreply.github.com>
2023-12-04 09:51:51 +0100

    Add day 04 puzzles solutions

--- src/Solutions/2023/Day04/Solver.cs
+++ src/Solutions/2023/Day04/Solver.cs
@@ -1,21 +1,54 @@
-﻿namespace Day04;
+﻿using System.Text.RegularExpressions;
 
-public class Solver : Solver<int, int>
+namespace Day04;
+
+public class Solver : Solver<List<Scratchcard>, int>
 {
     public Solver() : base("./Day04/input.txt") { }
 
-    public override int PartOne(int input)
+    public override int PartOne(List<Scratchcard> scratchcards)
+    {
+        return scratchcards.Select(sc =>
+        {
+            int result = 0;
+
+            int matchCount = sc.ScratchedNumbers.Count(sn => sc.WinningNumbers.Contains(sn));
+            for (int i = matchCount; i >= 1; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }).Sum();
+    }
+
+    public override int PartTwo(List<Scratchcard> scratchcards)
     {
         throw new NotImplementedException();
     }
 
-    public override int PartTwo(int input)
+    public override List<Scratchcard> ParseInput(IEnumerable<string> input)
     {
-        throw new NotImplementedException();
-    }
+        return input.Select(i =>
+        {
+            Match match = Regex.Match(i, @".+:\s+(?<winningNumbers>\d+.+)\|\s+(?<scratchedNumbers>\d+.+)");
 
-    public override int ParseInput(IEnumerable<string> input)
-    {
-        throw new NotImplementedException();
+            return new Scratchcard
+            {
+                WinningNumbers = ExtractNumbers(match.Groups["winningNumbers"].Value),
+                ScratchedNumbers = ExtractNumbers(match.Groups["scratchedNumbers"].Value)
+            };
+        }).ToList();
+
+        static List<int> ExtractNumbers(string input)
+        {
+            return input.Replace("  ", " ").Split(" ").Select(number => int.Parse(number)!).ToList();
+        }
     }
+}
+
+public class Scratchcard
+{
+    public List<int> WinningNumbers { get; set; }
+    public List<int> ScratchedNumbers { get; set; }
 }
--- src/Solutions/2023/Day04/SolverTest.cs
+++ src/Solutions/2023/Day04/SolverTest.cs
@@ -1,18 +1,36 @@
 ﻿namespace Day04;
 
-public class SolverTest : TestEngine<Solver, int, int>
+public class SolverTest : TestEngine<Solver, List<Scratchcard>, int>
 {
-    public override Puzzle PartOne => new() { ShouldSkipTests = true, Example = new(), Solution = 0 };
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+            RawInput =
+            [
+                "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+                "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+                "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+                "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+                "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+                "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+            ],
+            Input = 
+            [
+                new() { WinningNumbers = [41, 48, 83, 86, 17], ScratchedNumbers = [83, 86, 6, 31, 17, 9, 48, 53] },
+                new() { WinningNumbers = [13, 32, 20, 16, 61], ScratchedNumbers = [61, 30, 68, 82, 17, 32, 24, 19] },
+                new() { WinningNumbers = [1, 21, 53, 59, 44], ScratchedNumbers = [69, 82, 63, 72, 16, 21, 14, 1] },
+                new() { WinningNumbers = [41, 92, 73, 84, 69], ScratchedNumbers = [59, 84, 76, 51, 58, 5, 54, 83] },
+                new() { WinningNumbers = [87, 83, 26, 28, 32], ScratchedNumbers = [88, 30, 70, 12, 93, 22, 82, 36] },
+                new() { WinningNumbers = [31, 18, 13, 56, 72], ScratchedNumbers = [74, 77, 10, 23, 35, 67, 36, 11] }
+            ],
+            Result = 13
+        },
+        Solution = 0
+    };
 
     public override Puzzle PartTwo => new()
     {
-        ShouldSkipTests = true,
-        Example = new()
-        {
-            RawInput = [],
-            Input = 0,
-            Result = 0
-        },
-        Solution = 0
+        ShouldSkipTests = true, Example = new() { RawInput =  [], Input = 0, Result = 0 }, Solution = 0
     };
 }
