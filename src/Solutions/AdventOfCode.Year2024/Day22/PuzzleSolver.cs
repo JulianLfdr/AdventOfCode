5f9bd403282f6d3cd9c6e1476139b9618b4a866c
Julian Lefondeur <julian.lefondeur@gmail.com>
2025-03-22 18:29:33 +0100

    Add 2024 project

---
+++ src/Solutions/AdventOfCode.Year2024/Day22/PuzzleSolver.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Year2024.Day22;
+
+public sealed class PuzzleSolver : PuzzleSolver<int, int>
+{
+    public PuzzleSolver() : base(AdventOfCodeConstants.Days.Day22) { }
+
+    protected override Puzzle<int, int> PartOne
+        => throw new NotImplementedException();
+
+    protected override Puzzle<int, int> PartTwo
+        => throw new NotImplementedException();
+
+    protected override int ParseInput(IEnumerable<string> puzzleInput)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override int SolvePartOne(int input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override int SolvePartTwo(int input)
+    {
+        throw new NotImplementedException();
+    }
+}
